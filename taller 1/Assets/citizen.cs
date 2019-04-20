b3f79fee9dbc7fb0ae19f0706e7c5362ad434a5d
juan luis bermudez <juanluisber12@gmail.com>
2019-04-20 13:02:30 -0500

    primer taller

---
+++ taller 1/Assets/citizen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class citizen : MonoBehaviour
+{
+    public float moveX;
+    public float moveY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] Nombrezombie =
+        {
+           "elulalia","socorro","jose","antonio","pepe","saul"
+        };
+
+        string[] nombrecitizen =
+        {
+           "pablo","sergio","javier","david","carlos","damian","silvia","raquel","elena","boris","anibal","claudia",
+           "poncio","marina","mariela","pedro","paula","marcos","thomas","ediberto"
+
+        };
+        int asignar = UnityEngine.Random.Range(1, 5);
+
+        for(int i = 0; i < asignar; i++)
+        {
+
+            zombie zomb = new zombie(Nombrezombie[i], Random.Range(40, 98));
+            Debug.Log(zomb.stats());
+
+        }
+        int asignar2 = UnityEngine.Random.Range(1, 6);
+        for(int j = 0;j < asignar2; j++)
+        {
+           spawncitizen civ = new spawncitizen(nombrecitizen[UnityEngine.Random.Range(0, 20)], UnityEngine.Random.Range(15, 100));
+            Debug.Log(civ.Stats());
+        }
+        Hero hero = new Hero();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ taller 1/Assets/spawncitizen.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawncitizen 
+{
+    private string Nombre;
+    private int edad;
+    GameObject citizen;
+
+    public spawncitizen(string nombre,int edad)
+    {
+        this.Nombre = nombre;
+        this.edad = edad;
+        citizen = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        Vector3 posicion = new Vector3(UnityEngine.Random.Range(-20, 25), 1f, UnityEngine.Random.Range(-35, 30));
+        citizen.name = "Citizen";
+        citizen.transform.position = posicion;
+        citizen.GetComponent<Renderer>().material.color = Color.grey;
+
+    }
+    public string Stats()
+    {
+        string stats = "hola soy de la ciudad  me llamo:" + asignanombre() + " " + "tengo" + asignaedad() + "años";
+        return stats;
+
+    }
+
+    public string asignanombre()
+    {
+        return Nombre;
+    }
+    public void colocanombre(string nombre)
+    {
+        this.Nombre = nombre;
+    }
+    public int asignaedad()
+    {
+        return edad;
+    }
+    public void colocaedad(int edad)
+    {
+        this.edad = edad;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
